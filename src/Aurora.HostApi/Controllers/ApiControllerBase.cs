e40e6188a95491edcd82bbacd1de48eafe52c139
sealoyal <sealoyal@hotmail.com>
2022-11-26 22:35:25 +0800

    新增: ApiControllerBase

---
+++ src/Aurora.HostApi/Controllers/ApiControllerBase.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Aurora.HostApi.Controllers;
+
+/// <summary>
+/// 基础控制器
+/// </summary>
+[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+[Route("/api/[controller]/[action]")]
+[ApiController]
+public abstract class ApiControllerBase : ControllerBase
+{
+}
--- src/Aurora.HostApi/Controllers/System/AuthController.cs
+++ src/Aurora.HostApi/Controllers/System/AuthController.cs
@@ -1,12 +1,16 @@
 ﻿using Aurora.Application.Contracts.Systems;
 using Aurora.Application.Contracts.Systems.Dtos.AuthDtos;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using NSwag.Annotations;
 
 namespace Aurora.HostApi.Controllers.System;
 
-[ApiController]
-[Route("api/[controller]/[action]")]
-public class AuthController: ControllerBase
+/// <summary>
+/// 用户认证
+/// </summary>
+[OpenApiTag("用户验证")]
+public class AuthController: ApiControllerBase
 {
     private readonly IAuthAppService _authAppService;
 
@@ -21,9 +25,19 @@ public class AuthController: ControllerBase
     /// </summary>
     /// <param name="input">登录用户信息</param>
     /// <returns></returns>
-    [HttpPost]
+    [HttpPost,AllowAnonymous]
     public async Task<TokenDto> SignIn(SignInDto input)
     {
         return await _authAppService.SignInAsync(input);
+    }
+
+    /// <summary>
+    /// 测试
+    /// </summary>
+    /// <param name="input"></param>
+    [HttpPost]
+    public async Task SignOut(TokenDto input)
+    {
+        await Task.CompletedTask;
     }
 }
